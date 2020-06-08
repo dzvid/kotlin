d1fbc04213ee7bee97122a906c9f6189138798c3
David <oliveiradavid.dev@gmail.com>
2020-06-08 14:56:50 -0400

    feat: Generate random numbers between 0 and 50

--- randomicos/app/src/main/java/com/example/randomicos/MainActivity.kt
+++ randomicos/app/src/main/java/com/example/randomicos/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        if(v?.id == R.id.buttonRandom || v?.id == R.id.textNumber){
-
+        if (v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
+            val number = random()
         }
 
+    }
+
+    /*
+    * Returns an integer number between 0 and 50
+    */
+    fun random(): Int {
+        return Random.nextInt(until = 51)
     }
 }
