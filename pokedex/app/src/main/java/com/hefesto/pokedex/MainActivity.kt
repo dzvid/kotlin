a0d8fdccaf637f98df2d37b808761a007a7cb87c
David <oliveiradavid.dev@gmail.com>
2020-06-13 22:21:00 -0400

    feat: List pokemons screen

--- pokedex/app/src/main/java/com/hefesto/pokedex/MainActivity.kt
+++ pokedex/app/src/main/java/com/hefesto/pokedex/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hefesto.pokedex
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +8,49 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item_pokemon.view.*
 
 class MainActivity : AppCompatActivity() {
+    private var pokemons: List<Pokemon> =  listOf(
+        Pokemon(
+            "Pikachu",
+            25,
+            listOf("Eletric"),
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+            6f,
+            4f,
+            -3.1028263,
+            -60.0147652
+        ),
+        Pokemon(
+            "Squirtle",
+            7,
+            listOf("Water"),
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+            8.5f,
+            6f,
+            -1.9928572,
+            -60.0552653
+        ),
+        Pokemon(
+            "Charmander",
+            4,
+            listOf("Fire"),
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+            9f,
+            6f,
+            -3.091583,
+            -60.0198707
+        )
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val pokemons = listOf("Pikachu", "Voltorb", "Charmander")
+
         rvPokemons.adapter = PokemonAdapter(pokemons)
         shouldDisplayEmptyView(pokemons.isEmpty())
     }
@@ -23,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         emptyView.visibility = if(isEmpty) View.VISIBLE else View.GONE
     }
 
-    class PokemonAdapter(private val pokemons: List<String>): RecyclerView.Adapter< PokemonAdapter.PokemonViewHolder>(){
+    class PokemonAdapter(private val pokemons: List<Pokemon>): RecyclerView.Adapter< PokemonAdapter.PokemonViewHolder>(){
         class PokemonViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
@@ -33,8 +69,11 @@ class MainActivity : AppCompatActivity() {
 
         override fun getItemCount():Int = pokemons.size
 
+        @SuppressLint("SetTextI18n")
         override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-            holder.itemView.tvPokemonName.text = pokemons[position]
+            holder.itemView.tvPokemonName.text = pokemons[position].name
+            holder.itemView.tvPokemonNumber.text = "#%03d".format(pokemons[position].number)
+            Picasso.get().load(pokemons[position].imageUrl).into(holder.itemView.ivPokemonImage)
         }
     }
 }
