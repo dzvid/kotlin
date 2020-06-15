91c40fabcda6ef65cbb1c95e57e04dcc45cefdaa
David <oliveiradavid.dev@gmail.com>
2020-06-15 00:05:13 -0400

    Add: Pokemon layouts
    - Add pokemon screen
    - Pokemon Details

---
+++ pokedex/app/src/main/java/com/hefesto/pokedex/AddPokemonActivity.kt
@@ -0,0 +1,11 @@
+package com.hefesto.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pokemon)
+    }
+}
